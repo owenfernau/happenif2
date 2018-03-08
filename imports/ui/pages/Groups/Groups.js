ad126c73f13cc1acf05168fc6b24187ba2b1d9e1
acefox86 <owenfernau@gmail.com>
2018-03-08 14:56:00 -0800

    added groups and ideas ghost pages

---
+++ imports/ui/pages/Groups/Groups.js
@@ -0,0 +1,19 @@
+/*
+This is the page where users
+can see all the groups of which
+they're a member.
+
+Maybe they can also see
+minimal information about
+what's going on in that
+group. Like if there are
+ideas that are soon to
+"lock out."
+
+Users could also potentially
+see issues they had raised
+and what the sentiment was
+for those issues.
+
+
+*/
---
+++ imports/ui/pages/Ideas/Ideas.js
@@ -0,0 +1,14 @@
+/*
+
+Okay so this is where
+the user can view the
+ideas they raised and
+potentially the ideas
+they have voted on and
+maybe also the ones they
+"need" to vote on.
+
+
+
+
+*/
