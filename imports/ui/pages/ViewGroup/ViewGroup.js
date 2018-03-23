88e9d9f8526031106198435a2ae0f23823819329
acefox86 <owenfernau@gmail.com>
2018-03-23 13:54:03 -0700

    struggling with groups

--- imports/ui/pages/ViewGroup/ViewGroup.js
+++ imports/ui/pages/ViewGroup/ViewGroup.js
@@ -4,7 +4,7 @@ import { ButtonToolbar, ButtonGroup, Button } from 'react-bootstrap';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
-import GroupsCollection from '../../../api/Groups/Groups';
+import Groups from '../../../api/Groups/Groups';
 import NotFound from '../NotFound/NotFound';
 import Loading from '../../components/Loading/Loading';
 
@@ -34,9 +34,9 @@ ViewGroup.propTypes = {
 export default withTracker(({ match }) => {
   const groupId = match.params._id;
   const subscription = Meteor.subscribe('groups.view', groupId);
-
+  console.log(Groups.findOne(groupId))
   return {
     loading: !subscription.ready(),
-    groups: GroupsCollection.find().fetch(),
+    groups: Groups.findOne(groupId),
   };
 })(ViewGroup);
